a93fe51ecacb777b16761620b81c0604cadf07fa
Mbasa <mbasacokile7@gmail.com>
2026-02-17 16:09:57 +0200

    Render main UI and add count state value update functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,36 @@
+import { useState } from "react";
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  // Setup a count state value
+  const [count, setCount] = useState(1);
+
+  //OnChange Function
+  function handleChange(e) {
+    setCount(() => {
+      const newCount = e.target.value;
+      return newCount;
+    });
+  }
+
+  // Handle Submit Function
+  return (
+    <section className="section-center">
+      <h4>Tired of boring lorem ipsum?</h4>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="number">Paragraphs: </label>
+        <input
+          type="number"
+          name="number"
+          id="number"
+          min={1}
+          max={8}
+          step={1}
+          value={count}
+          onChange={handleChange}
+        />
+        <button className="btn">Generate</button>
+      </form>
+    </section>
+  );
 };
 export default App;
